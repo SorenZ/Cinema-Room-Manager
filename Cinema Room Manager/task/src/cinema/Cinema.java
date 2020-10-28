5d9a994bb7ed06ac06871a5ea2918df88752ba13
Hamed ZVand <sunrise.freedom@gmail.com>
2020-10-28 19:35:33 +0330

    calculate seat price

--- Cinema Room Manager/task/src/cinema/Cinema.java
+++ Cinema Room Manager/task/src/cinema/Cinema.java
@@ -14,8 +14,20 @@ public class Cinema {
         System.out.println("Enter the number of seats in each row:");
         int columns = scanner.nextInt();
 
-        System.out.println("Total income:");
-        System.out.println("$" + calculateIncome(rows, columns));
+        printSeats(rows, columns, 0, 0);
+
+        System.out.println("Enter a row number:");
+        int occupiedRow = scanner.nextInt();
+
+        System.out.println("Enter a seat number in that row:");
+        int occupiedColumn = scanner.nextInt();
+
+        System.out.println("Ticket price: $" + calculateSeatPrice(rows,columns, occupiedRow, occupiedColumn));
+        System.out.println();
+        printSeats(rows, columns, occupiedRow, occupiedColumn);
+
+//        System.out.println("Total income:");
+//        System.out.println("$" + calculateIncome(rows, columns));
 
     }
 
@@ -32,11 +44,37 @@ public class Cinema {
         return firstHalf * columns * 10 + secondHalf * columns * 8;
     }
 
-    private static void printSeats() {
+    private static int calculateSeatPrice(int rows, int columns, int occupiedRow, int occupiedColumn) {
+        var totalSeats = rows * columns;
+
+        if(totalSeats <= 60)
+            return 10;
+
+        int firstHalf = rows / 2;
+
+        int secondHalf = firstHalf + (((rows % 2) == 0) ? 0 : 1);
+
+        return (occupiedRow <= firstHalf)
+                ? 10
+                : 8;
+    }
+
+    private static void printSeats(int rows, int columns, int occupiedRow, int occupiedColumn) {
         System.out.println("cinema.Cinema:");
-        System.out.println("  1 2 3 4 5 6 7 8");
-        for (int i = 1; i < 8; i++) {
-            System.out.println(String.format("%d S S S S S S S S",i));
+
+        System.out.print(" ");
+        for (int i = 1; i <= columns; i++) {
+            System.out.print(" " + i);
+        }
+
+        System.out.println();
+
+        for (int i = 1; i <= rows; i++) {
+            System.out.print(i);
+            for (int j = 1; j <= columns; j++) {
+                System.out.print((occupiedRow == i && occupiedColumn == j) ? " B" : " S");
+            }
+            System.out.println();
         }
     }
 }
